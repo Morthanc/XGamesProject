5ed661b6c76f284ea30b0e93afd4166a8c0f967a
magnumveras <magno.veras88@gmail.com>
2017-05-18 11:10:22 -0300

    Exclusão da Classe Endereço e Inclusão da mesma na Classe Cliente

--- XGamesProject/src/com/senac/xgames/model/Cliente.java
+++ XGamesProject/src/com/senac/xgames/model/Cliente.java
@@ -12,9 +12,73 @@ public class Cliente {
     private String telefone1;
     private String telefone2;
     private String email;
+    private String logradouro;
+    private String bairro;
+    private String complemento;
+    private String cep;
+    private String numero;
+    private String cidade;
+    private String estado;
+    
     
     public int getId() {
         return id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 
     public void setId(int id) {
