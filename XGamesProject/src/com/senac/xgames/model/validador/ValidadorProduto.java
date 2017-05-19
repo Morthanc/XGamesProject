64dbd436c896e8d8baeaffc68fc7782fe7684103
magnumveras <magno.veras88@gmail.com>
2017-05-19 09:23:10 -0300

    Criação de Model para Validações

---
+++ XGamesProject/src/com/senac/xgames/model/validador/ValidadorProduto.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.senac.xgames.model.validador;
+import com.senac.xgames.model.Produto;
+/**
+ *
+ * @author Magno
+ */
+public class ValidadorProduto {
+    public static void validar(Produto produto){
+        
+    }
+}
---
+++ XGamesProject/src/com/senac/xgames/model/validador/ValidadorVenda.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.senac.xgames.model.validador;
+import com.senac.xgames.model.Venda;
+import com.senac.xgames.model.ItemVenda;
+/**
+ *
+ * @author Magno
+ */
+public class ValidadorVenda {
+    
+}
