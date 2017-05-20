6795319aac3c5a33ee54c36e6f53bb7f72ae9c05
magno <magno@magno>
2017-05-19 23:41:32 -0300

    Implementacao da Classe ServicoProduto

--- XGamesProject/src/com/senac/xgames/service/ServicoProduto.java
+++ XGamesProject/src/com/senac/xgames/service/ServicoProduto.java
@@ -4,11 +4,100 @@
  * and open the template in the editor.
  */
 package com.senac.xgames.service;
-
+import com.senac.xgames.exceptions.ProdutoException;
+import com.senac.xgames.model.Produto;
+import com.senac.xgames.model.validador.ValidadorProduto;
+import com.senac.xgames.mock.MockProduto;
+import com.senac.xgames.exceptions.DataSourceException;
+import java.util.List;
 /**
  *
  * @author magno
  */
+//Classe de Servico de Produto
 public class ServicoProduto {
-    
+    //Insere um Produto na fonte de dados
+    public static void cadastrarProduto(Produto produto)
+            throws ProdutoException, DataSourceException {
+
+        //Chama o validador para verificar o produto
+        ValidadorProduto.validar(produto);
+
+        try {
+            //Realiza a chamada de inserção na fonte de dados
+            MockProduto.inserir(produto);
+        } catch (Exception e) {
+            //Imprime qualquer erro técnico no console e devolve
+            //uma exceção e uma mensagem amigável a camada de visão
+            e.printStackTrace();
+            throw new DataSourceException("Erro na fonte de dados", e);
+        }
+    }
+
+    //Atualiza um produto na fonte de dados
+    public static void atualizarProduto(Produto produto)
+            throws ProdutoException, DataSourceException {
+        
+        //Chama o validador para verificar o produto
+        ValidadorProduto.validar(produto);
+
+        try {
+            //Realiza a chamada de atualização na fonte de dados
+            MockProduto.atualizar(produto);
+            return;
+        } catch (Exception e) {
+            //Imprime qualquer erro técnico no console e devolve
+            //uma exceção e uma mensagem amigável a camada de visão
+            e.printStackTrace();
+            throw new DataSourceException("Erro na fonte de dados", e);
+        }
+    }
+
+    //Realiza a pesquisa de um produto por nome na fonte de dados
+    public static List<Produto> procurarProduto(String nome)
+            throws ProdutoException, DataSourceException {
+        try {
+            //Verifica se um parâmetro de pesquisa não foi informado.
+            //Caso afirmativo, realiza uma listagem simples do mock.
+            //Caso contrário, realiza uma pesquisa com o parâmetro
+            if (nome == null || "".equals(nome)) {
+                return MockProduto.listar();
+            } else {
+                return MockProduto.procurar(nome);
+            }
+        } catch (Exception e) {
+            //Imprime qualquer erro técnico no console e devolve
+            //uma exceção e uma mensagem amigável a camada de visão
+            e.printStackTrace();
+            throw new DataSourceException("Erro na fonte de dados", e);
+        }
+    }
+
+    //Obtem o produto com ID informado do mock
+    public static Produto obterProduto(Integer id)
+            throws ProdutoException, DataSourceException {
+        try {
+            
+            return MockProduto.obter(id);
+        } catch (Exception e) {
+            //Imprime qualquer erro técnico no console e devolve
+            //uma exceção e uma mensagem amigável a camada de visão
+            e.printStackTrace();
+            throw new DataSourceException("Erro na fonte de dados", e);
+        }
+    }
+
+    //Exclui o cliente com ID informado do mock
+    public static void excluirProduto(Integer id)
+            throws ProdutoException, DataSourceException {
+        try {
+            
+            MockProduto.excluir(id);
+        } catch (Exception e) {
+            //Imprime qualquer erro técnico no console e devolve
+            //uma exceção e uma mensagem amigável a camada de visão
+            e.printStackTrace();
+            throw new DataSourceException("Erro na fonte de dados", e);
+        }
+    }
 }
