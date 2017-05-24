7cbee01b284f97e3de13a06cbcc92a6e0d2c3eaf
Matheus Matias <matheus.araujo1310.mm@outlook.com>
2017-05-24 17:04:39 -0300

    Criação Mock Venda e Mock ItemVenda

---
+++ XGamesProject/src/com/senac/xgames/mock/MockVenda.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.senac.xgames.mock;
+
+import com.senac.xgames.model.Venda;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Magno
+ */
+public class MockVenda {
+    //Variável serve para controlar o número de venda
+    private static int totalVenda = 0;
+    
+    //Armazena uma lista de vendas para manipulação no Mock
+    private static List<Venda> listaVendas = new ArrayList<Venda>();
+    
+    //Insere uma nova venda no Mock
+    public static void inserir(Venda venda) throws Exception{
+        venda.setCodigo(totalVenda++);
+        listaVendas.add(venda);
+    }
+    
+    
+}
