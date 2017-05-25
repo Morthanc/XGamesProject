cdf909f9a9303dd7b20cbe4433f7fe4ade62971d
magno <magno@magno>
2017-05-24 21:19:28 -0300

    Conserto Validador venda

--- XGamesProject/src/com/senac/xgames/model/validador/ValidadorVenda.java
+++ XGamesProject/src/com/senac/xgames/model/validador/ValidadorVenda.java
@@ -4,38 +4,27 @@
  * and open the template in the editor.
  */
 package com.senac.xgames.model.validador;
-import com.senac.xgames.exceptions.ClienteException;
 import com.senac.xgames.exceptions.VendaException;
-import com.senac.xgames.exceptions.ProdutoException;
 import com.senac.xgames.exceptions.ItemVendaException;
-import com.senac.xgames.model.Cliente;
 import com.senac.xgames.model.Venda;
 import com.senac.xgames.model.ItemVenda;
-import com.senac.xgames.model.Produto;
+
 /**
  *
  * @author Magno
  */
 public class ValidadorVenda {
-    public static Cliente cliente = new Cliente();
     public static ItemVenda itemvenda = new ItemVenda();
-    public static Produto produto = new Produto();
     
-    public static void validar(Venda venda) throws VendaException, ClienteException, ItemVendaException, ProdutoException{
+    public static void validar(Venda venda) throws VendaException, ItemVendaException{
         
-         if(cliente == null){
-            throw new ClienteException("Não foi informado um cliente");
+         if(venda.getNomeCliente() == null){
+            throw new VendaException("Não foi informado um cliente");
         }
          
-         if(itemvenda == null){
-             throw new ItemVendaException("Não foi adicionado um produto");
-         }
-         
-         if(produto.getEstoque() == 0 || produto.getEstoque() == null){
-             throw  new ProdutoException("Não existe o produto em estoque");
-         }
-         
-         
+         if(venda.getListaItemVenda() == null){
+             throw new VendaException("Não foi adicionado nenhum produto");
+         }     
          
         
     }
